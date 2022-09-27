e6966ae1b6b4e74afd6b77a481a8d4a3d291ec5f
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2022-09-27 12:58:54 -0400

    Update plotConditioningCalib.m

--- plotConditioningCalib.m
+++ plotConditioningCalib.m
@@ -6,9 +6,9 @@ function [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,
 for k = first_analysis_day:length(files)
     % First, determine the analysis window
     if strcmp(rig{k},'black') == 1
-        win{k} = [108 109 110 111]; 
+        win{k} = [139 140 141 142]; 
     elseif strcmp(rig{k},'blue') == 1
-        win{k} = [35 36 37 38];
+        win{k} = [47 48 49 50];
     end  
     
     % Find the start and endpoints of the analysis window
@@ -32,13 +32,13 @@ for k = first_analysis_day:length(files)
         if strcmp(rig{k},'black') == 1
            xline(68,'b'); xline(151,'b'); % 2018b and newer
     %        l1 = line([68 68], [0 1]); set(l1,'color','b'); l2 = line([151 151], [0 1]); set(l2,'color','b');
-           xline(112,'g'); xline(119,'g'); % 2018b and newer
-    %        l1 = line([112 112], [0 1]); set(l1,'color','g'); l2 = line([119 119], [0 1]); set(l2,'color','g');
+           xline(143,'g'); xline(153,'g'); % 2018b and newer
+    %        l1 = line([143 143], [0 1]); set(l1,'color','g'); l2 = line([153 153], [0 1]); set(l2,'color','g');
         elseif strcmp(rig{k},'blue') == 1
             xline(24,'b'); xline(53,'b'); % 2018b and newer
     %         l1 = line([24 24], [0 1]); set(l1,'color','b'); l2 = line([53 53], [0 1]); set(l2,'color','b');
-            xline(39,'g'); xline(42,'g'); % 2018b and newer
-    %         l1 = line([39 39], [0 1]); set(l1,'color','g'); l2 = line([42 42], [0 1]); set(l2,'color','g');            
+            xline(51,'g'); xline(54,'g'); % 2018b and newer
+    %         l1 = line([51 51], [0 1]); set(l1,'color','g'); l2 = line([54 54], [0 1]); set(l2,'color','g');            
         end
         title([mouse ' ' num2str(date{k}) ' (Day ' num2str(k) ') average of ' num2str(size(eyelid3_5_trials{k},1)) ' CS-US trials']);
         ylim([0 1]); xlim([0 size(trials{1,1}.tm,2)]); ylabel('FEC'); 
@@ -49,11 +49,11 @@ for k = first_analysis_day:length(files)
     %         l1 = line([start_analysis start_analysis], [0 1]); set(l1,'color','k'); l2 = line([end_analysis end_analysis], [0 1]); set(l2,'color','k'); 
         end
         if strcmp(rig{k},'black') == 1
-           xline(112,'g'); xline(119,'g'); % 2018b and newer
-    %        l1 = line([112 112], [0 1]); set(l1,'color','g'); l2 = line([119 119], [0 1]); set(l2,'color','g');
+           xline(143,'g'); xline(153,'g'); % 2018b and newer
+    %        l1 = line([143 143], [0 1]); set(l1,'color','g'); l2 = line([153 153], [0 1]); set(l2,'color','g');
         elseif strcmp(rig{k},'blue') == 1
-            xline(39,'g'); xline(42,'g'); % 2018b and newer
-    %         l1 = line([39 39], [0 1]); set(l1,'color','g'); l2 = line([42 42], [0 1]); set(l2,'color','g');            
+            xline(51,'g'); xline(54,'g'); % 2018b and newer
+    %         l1 = line([51 51], [0 1]); set(l1,'color','g'); l2 = line([54 54], [0 1]); set(l2,'color','g');            
         end
         title([mouse ' ' num2str(date{k}) ' (Day ' num2str(k) ') average of ' num2str(size(eyelid3_7_trials{k},1)) ' US-only trials']); 
         ylim([0 1]); xlim([0 size(trials{1,1}.tm,2)]); ylabel('FEC'); xlabel('Frame');
