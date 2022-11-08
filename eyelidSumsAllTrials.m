8604e19240676c8af263d9a1cdd9f9ebbe1f3a48
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2022-11-07 22:54:39 -0500

    Add files via upload

---
+++ eyelidSumsAllTrials.m
@@ -0,0 +1,124 @@
+%% eyelidSumsAllTrials TEST %%
+% Binned CRprobs and CRamps across all trials
+
+% Written by Kayla Fernando (10/27/22)
+
+clear all
+close all
+clc
+
+mouse = 'KF64'; 
+basepath = 'Y:\\home\kayla\Eyelid conditioning\';
+
+% Preprocess eyelid conditioning data, output promptData.txt
+eyelidPreprocess
+
+% Define trial type to analyze
+if numel(unique(rig)) == 1
+    trialType = cspaired_all;
+elseif numel(unique(rig)) > 1
+    trialType = cspaired_all_cell;
+end
+
+% Block process the array to replace every element in the 100 element-wide block by the mean of the values in the block
+    % First, define the averaging function for use by blockproc()
+    meanFilterFunction = @(block_struct) mean(block_struct.data);
+    % Define the block parameters (m rows by n cols block). We will average every m trials
+    for k = 1:length(files)
+        if strcmp(rig{k},'black') == 1
+            blockSize{k} = [100 334];
+        elseif strcmp(rig{k},'blue') == 1
+            blockSize{k} = [100 200];
+        end
+    end
+    % Now do the actual averaging (block average down to smaller size array)
+    if numel(unique(rig)) == 1
+        for k = 1:length(files)
+            blockAveragedDownSignal = blockproc(trialType, blockSize{k}, meanFilterFunction);
+        end
+        % Let's check the output size
+        [bins, frames] = size(blockAveragedDownSignal)
+    elseif numel(unique(rig)) > 1
+        for k = 1:length(files)
+            blockAveragedDownSignal{k} = blockproc(trialType{k}, blockSize{k}, meanFilterFunction);
+            frames(k) = size(blockAveragedDownSignal{k},2);
+        end
+        % Let's check the output size
+        bins = size(blockAveragedDownSignal{1},1)
+        frames = unique(frames)
+    end
+    
+    % Also define the summation function for use by blocproc()
+    sumFunction = @(block_struct) sum(block_struct.data);
+  
+% Calculate binned CRprobs across all trials using all CS-US trials
+if numel(unique(rig)) == 1
+    % Binned CRamp
+        if strcmp(rig,'black') == 1
+            win = [139 140 141 142];
+            cramp = mean(trialType(:,win),2) - mean(trialType(:,1:66),2); 
+        elseif strcmp(rig,'blue') == 1
+            win = [47 48 49 50];
+            cramp = mean(trialType(:,win),2) - mean(trialType(:,1:10),2); 
+        end
+        blockSize2 = [100 1];
+        CRamps = blockproc(cramp, blockSize2, meanFilterFunction);
+    % Binned CRprob
+        cramp2 = cramp>0.1;
+        blockSums = blockproc(cramp2, blockSize2, sumFunction);
+        for k = 1:length(files)
+            blockSizeTemp = blockSize{k};
+            CRprobs = blockSums./blockSizeTemp(1);
+        end
+elseif numel(unique(rig)) > 1
+    for k = 1:length(files)
+        % Binned CRamp
+            if strcmp(rig{k},'black') == 1
+                win{k} = [139 140 141 142];
+                trialTypeTemp = trialType{k};
+                cramp{k} = mean(trialTypeTemp(:,win{k}),2) - mean(trialTypeTemp(:,1:66),2); 
+            elseif strcmp(rig{k},'blue') == 1
+                win{k} = [47 48 49 50];
+                trialTypeTemp = trialType{k};
+                cramp{k} = mean(trialTypeTemp(:,win{k}),2) - mean(trialTypeTemp(:,1:10),2); 
+            end
+    end
+            cramp = vertcat(cramp{:});
+            blockSize2 = [100 1];
+            CRamps = blockproc(cramp, blockSize2, meanFilterFunction);
+        % Binned CRprob
+            cramp2 = cramp>0.1;
+            blockSums = blockproc(cramp2, blockSize2, sumFunction);
+            for k = 1:length(files)
+                blockSizeTemp = blockSize{k};
+                CRprobs = blockSums./blockSizeTemp(1);
+            end
+end
+
+% Plot binned eyelid traces as heatmap and binned CRprobs across all trials
+[h,hf1] = plotBlockProcessedTrials(blockAveragedDownSignal,mouse,rig,files,trials,blockSizeTemp,CRamps,CRprobs);
+    
+% Calculating binned CRamps across all trials using only successful CS-US trials 
+[keep_cramp] = sortTrials(rig,win,trialType,files);
+if iscell(keep_cramp) == 1
+    keep_cramp = cell2mat(keep_cramp');
+end
+
+% Block process the array to replace every element in the 100 element-wide block by the mean of the values in the block
+    % First, define the averaging function for use by blockproc()
+    meanFilterFunctionAmps = @(block_struct) mean(block_struct.data);
+    % Define the block parameters (m rows by n cols block). We will average every m trials
+    blockSize3 = [50 1];
+    if numel(keep_cramp) < 50
+        blockSize3 = [round(numel(keep_cramp),1,"significant")/5 1];
+    end
+    blockAveragedDownAmps = blockproc(keep_cramp, blockSize3, meanFilterFunctionAmps);
+
+% Plot binned CRamp learning curves
+figure;
+hf2 = plot(blockAveragedDownAmps);
+title([mouse ' CRamp across all trials']);
+xlabel(['Trial block (' num2str(blockSize3(1)) ' trials each)']);
+ylabel('FEC');
+xlim([0 size(blockAveragedDownAmps,1)]); ylim([0 1]);
+set(gca,'ytick',0:0.1:1);
---
+++ eyelidSumsPerSession.m
@@ -0,0 +1,51 @@
+%% eyelidSumsPerSession TEST %%
+% Plotting average eyelid traces for CS-US trials, US-only trials, and CS-catch trials over all sessions
+
+% Written by Kayla Fernando (10/27/22)
+
+% NOTE: Plotting the average eyelid traces for each trial type only
+% uses data from s01, so days with multiple sessions won't be plotted
+% properly. Analysis with multiple sessions works fine.
+
+clear all
+close all
+clc
+
+mouse = 'KF51'; 
+basepath = 'Y:\\home\kayla\Eyelid conditioning\';
+
+% Preprocess eyelid conditioning data, output promptData.txt
+eyelidPreprocess
+
+switch promptData
+    % No multiple sessions, different rigs
+    case '0  0'
+        [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,files,trials,date,1);
+        [ax3,ax4,win] = plotConditioningCatch(conditioning_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,win,files,trials,date,1);
+        [ax5,ax6,ax7,win] = plotConditioningCalibCatch(conditioning_trials,calib_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,eyelid3_0_trials,mouse,rig,win,files,trials,date,1);
+    % No multiple sessions, same rig, blue
+    case '0  1  0'
+        [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,files,trials,date,1);
+        [ax3,ax4,win] = plotConditioningCatch(conditioning_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,win,files,trials,date,1);
+        [ax5,ax6,ax7,win] = plotConditioningCalibCatch(conditioning_trials,calib_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,eyelid3_0_trials,mouse,rig,win,files,trials,date,1);
+    % No multiple sessions, same rig, black
+    case '0  1  1'
+        [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,files,trials,date,1);
+        [ax3,ax4,win] = plotConditioningCatch(conditioning_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,win,files,trials,date,1);
+        [ax5,ax6,ax7,win] = plotConditioningCalibCatch(conditioning_trials,calib_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,eyelid3_0_trials,mouse,rig,win,files,trials,date,1);
+    % Multiple sessions, different rigs
+    case '1  0'
+        [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,files,trials,date,1);
+        [ax3,ax4,win] = plotConditioningCatch(conditioning_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,win,files,trials,date,1);
+        [ax5,ax6,ax7,win] = plotConditioningCalibCatch(conditioning_trials,calib_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,eyelid3_0_trials,mouse,rig,win,files,trials,date,1);
+    % Multiple sessions, same rig, blue
+    case '1  1  0'
+        [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,files,trials,date,1);
+        [ax3,ax4,win] = plotConditioningCatch(conditioning_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,win,files,trials,date,1);
+        [ax5,ax6,ax7,win] = plotConditioningCalibCatch(conditioning_trials,calib_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,eyelid3_0_trials,mouse,rig,win,files,trials,date,1);
+    % Multiple sessions, same rig, black
+    case '1  1  1'
+        [ax1,ax2,win] = plotConditioningCalib(conditioning_trials,calib_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,files,trials,date,1);
+        [ax3,ax4,win] = plotConditioningCatch(conditioning_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,mouse,rig,win,files,trials,date,1);
+        [ax5,ax6,ax7,win] = plotConditioningCalibCatch(conditioning_trials,calib_trials,catch_trials,eyelid3_5_trials,eyelid3_7_trials,eyelid3_0_trials,mouse,rig,win,files,trials,date,1);
+end
