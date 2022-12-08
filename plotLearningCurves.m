f9472d3007bb6db5cc9ab1d86b0c26aa735db84b
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2022-12-08 14:32:16 -0500

    Update plotLearningCurves.m

--- plotLearningCurves.m
+++ plotLearningCurves.m
@@ -16,9 +16,9 @@ saline_CRamps(saline_CRamps == 0) = NaN;
 % Plot binned CRprobs for all ISRIB mice across all trials
 h1 = figure; title('CRprob across all trials'); xlabel('Trial block (100 trials each)'); ylabel('Probability'); ylim([0 1]); xlim([0 20]);
 shadedErrorBar(1:size(ISRIB_CRprobs,1),mean(ISRIB_CRprobs,2,'omitnan'),std(ISRIB_CRprobs,0,2,'omitnan'),'lineProps','r'); hold on
-h2 = shadedErrorBar(1:size(saline_CRprobs,1),mean(saline_CRprobs,2,'omitnan'),std(saline_CRprobs,0,2,'omitnan')); hold off
+h2 = shadedErrorBar(1:size(saline_CRprobs,1),mean(saline_CRprobs,2,'omitnan'),std(saline_CRprobs,0,2,'omitnan'),'lineProps','b'); hold off
 
 % Plot binned CRamps for all saline mice across all trials
 h3 = figure; title('CRamp across all trials'); xlabel('Trial block (100 trials each)'); ylabel('FEC'); ylim([0 0.5]); xlim([0 20]);
 shadedErrorBar(1:size(ISRIB_CRamps,1),mean(ISRIB_CRamps,2,'omitnan'),std(ISRIB_CRamps,0,2,'omitnan'),'lineProps','r'); hold on
-h4 = shadedErrorBar(1:size(saline_CRamps,1),mean(saline_CRamps,2,'omitnan'),std(saline_CRamps,0,2,'omitnan')); hold off
+h4 = shadedErrorBar(1:size(saline_CRamps,1),mean(saline_CRamps,2,'omitnan'),std(saline_CRamps,0,2,'omitnan'),'lineProps','b'); hold off
