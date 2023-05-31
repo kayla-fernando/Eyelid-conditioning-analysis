4bfa7fdbb5f18b09b0b3199dabb4dd30f2712656
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-05-31 16:13:49 -0400

    Update resampleRealignforVis.m

--- resampleRealignforVis.m
+++ resampleRealignforVis.m
@@ -6,32 +6,42 @@ close all
 clear all
 clc
 
-% Load workspace (average eyelid trace for naive, chance, or learned
+% Load workspace (average eyelid trace for naive, intermediate, or learned
 % condition for each animal in a given experimental condition. 1 row = 1
 % animal
 load('workspace.mat');
+load('times.mat')
 group_learned(group_learned == 0) = NaN;
+
+color = [0.6350 0.0780 0.1840];
+r1 = rectangle('Position',[times(68) 0 0.250 1],'FaceColor','b','EdgeColor','none'); hold on
+%r2 = rectangle('Position',[times(143) 0 0.030 1],'FaceColor','g','EdgeColor','none'); hold on
 
 for n = 1:size(group_learned,1)
     if any(isnan(group_learned(n,:))) == 1
         dataTemp = group_learned(n,1:200);
         dataTempResample = resample(dataTemp,5,3);
-        plot(horzcat(zeros(1,60),dataTempResample)); 
+        plot(times,horzcat(NaN(1,60),dataTempResample(1,1:274)),'Color',color); 
         hold on
     else
-        plot(group_learned(n,:))
+        plot(times,group_learned(n,:),'Color',color)
         hold on
     end
 end
 
-if version('-release') == '2017b'
-    l1 = line([68 68], [0 1]); set(l1,'color','b'); l2 = line([151 151], [0 1]); set(l2,'color','b');
-    l1 = line([143 143], [0 1]); set(l1,'color','g'); l2 = line([153 153], [0 1]); set(l2,'color','g');
-else
-    xline(gca,68,'b-','Alpha',1); xline(gca,151,'b-','Alpha',1); % 2018b and newer
-    xline(gca,143,'g-','Alpha',1); xline(gca,153,'g-','Alpha',1); % 2018b and newer
-end
-
-xlim([1 334]); ylim([0 1]);
-xlabel('Frame (realigned)'); ylabel('FEC (resampled)');
+xlabel('Time from CS (s)'); ylabel('FEC (resampled)'); ylim([0 1]); set(gca,'ytick',0:0.5:1);
 hold off
+
+%% 
+
+% run eyelidSumsAllTrials for a mouse that was trained on different rigs
+
+% for k = 1:length(keep_trials)
+%     temp = keep_trials{k};
+%     for n = 1:size(temp,1)
+%         [row,col] = find(cspaired_all_cell{k} == temp(n,:));
+%         t = cspaired_all_cell{k};
+%         t(row(1),:)
+%         pause
+%     end
+% end
