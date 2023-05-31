a1fc1f5072505dad5a02a5099d8abc64a7c2447c
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-05-31 16:04:53 -0400

    Update plotBlockProcessedSeqCRamps.m

--- plotBlockProcessedSeqCRamps.m
+++ plotBlockProcessedSeqCRamps.m
@@ -1,4 +1,4 @@
-function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_cramp,mouse,blockSizeTemp,c)
+function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_cramp,fullCR,mouse,blockSizeTemp,c)
 % Takes successful trials and their corresponding CR amplitudes, bins them 
 % into user-defined number of trial blocks while preserving the temporal 
 % structure of training, then finds the average within that trial block. 
@@ -8,13 +8,20 @@ function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_
 %       numBlocks = number of trial blocks 
 %       keep_trials_idx = index of trials that had a detected CR
 %       keep_cramp = CRamp values from those successful trials
-% OUTPUT:
+%       fullCR = full CRamp values (baseline + keep_cramp) from those successful trials
+% OUTPUTS:
 %       blockCRamps = average CRamp at each trial block, 
+%           calculated using only CRamps from successful trials within a trial 
+%           block, preserving temporal structure of training
+%       blockFullCRamps = average full CRamp at each trial block,
 %           calculated using only CRamps from successful trials within a trial 
 %           block, preserving temporal structure of training
 %       hf2 = plot binned CRamps using only successful CS-US trials while 
 %           preserving temporal structure of training
-%       hf3 = plot each detected CRamp as it occurs throughout training
+%       hf3 = plot binned full CRamps only successful CS-US trials while 
+%           preserving temporal structure of training
+%       hf4 = plot each detected CRamp as it occurs throughout training
+%       hf5 = plot each full detected CRamp as it occurs throughout training
 
 % Written by Kayla Fernando (3/28/23)
 
@@ -26,8 +33,10 @@ while n < numBlocks-1
     idx = find(keep_trials_idx >= binRange(1) & keep_trials_idx < binRange(2)); % does not include right edge
     if isempty(idx)
         blockCRamps(n+1) = 0;
+        blockFullCRamps(n+1) = 0;
     else
         blockCRamps(n+1) = mean(keep_cramp(idx(1):idx(end)));
+        blockFullCRamps(n+1) = mean(fullCR(idx(1):idx(end)));
     end
     n = n+1;
 end
@@ -38,31 +47,51 @@ if n == numBlocks-1
     idx = find(keep_trials_idx >= binRange(1) & keep_trials_idx <= binRange(2)); % includes right edge
     if isempty(idx)
         blockCRamps(20) = 0;
+        blockFullCRamps(20) = 0;
     else
         blockCRamps(20) = mean(keep_cramp(idx(1):idx(end)));
+        blockFullCRamps(20) = mean(fullCR(idx(1):idx(end)));
     end
 end
 
 % Plot binned CRamps using only successful CS-US trials while preserving temporal structure of training
 figure;
-hf2 = plot(blockCRamps);
-title([mouse ' CRamps of detected CRs in sequential order']);
-xlim([0 20]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
-ylim([0 1]); ylabel('Fraction of eye closed');
+ax1 = subplot(2,1,1);
+    hf2 = plot(blockCRamps);
+    title([mouse ' CRamps of detected CRs in sequential order']);
+    xlim([0 20]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
+    ylim([0 1]); ylabel('Fraction of eye closed');
+ax2 = subplot(2,1,2);
+    hf3 = plot(blockFullCRamps);
+    title([mouse ' full CRamps of detected CRs in sequential order']);
+    xlim([0 20]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
+    ylim([0 1]); ylabel('Fraction of eye closed');
+linkaxes([ax1, ax2], 'x');
 
 % Plot each detected CRamp as it occurs throughout training
 figure;
-hf3 = scatter(keep_trials_idx,keep_cramp,'.',c);
-title([mouse ' all CRamps of detected CRs in sequential order']);
-xlim([0 2000]); xlabel('Trial #');
-ylim([0 1]); ylabel('Fraction of eye closed');
+ax3 = subplot(2,1,1);
+    hf4 = scatter(keep_trials_idx,keep_cramp,'.',c);
+    title([mouse ' all CRamps of detected CRs in sequential order']);
+    xlim([0 2000]); xlabel('Trial #');
+    ylim([0 1]); ylabel('Fraction of eye closed');
+ax4 = subplot(2,1,2);
+    hf5 = scatter(keep_trials_idx,fullCR,'.',c);
+    title([mouse ' all full CRamps of detected CRs in sequential order']);
+    xlim([0 2000]); xlabel('Trial #');
+    ylim([0 1]); ylabel('Fraction of eye closed');
+linkaxes([ax3, ax4], 'x');
 
-if nargout == 1
+if nargout == 2
     varargout{1} = blockCRamps;
+    varargout{2} = blockFullCRamps;
 elseif nargout > 1
     varargout{1} = blockCRamps;
-    varargout{2} = hf2;
-    varargout{3} = hf3;
+    varargout{2} = blockFullCRamps;
+    varargout{3} = hf2;
+    varargout{4} = hf3;
+    varargout{5} = hf4;
+    varargout{6} = hf5;
 end
     
 end
