c339cd07bf979818e642a1219e54e248306b9a1a
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-06-01 08:38:29 -0400

    Update sortTrials.m

--- sortTrials.m
+++ sortTrials.m
@@ -17,9 +17,9 @@ if numel(unique(rig)) == 1
         keep_trials_trialspace = keep_trials;
         keep_trials_idx = find(~cellfun('isempty',keep_trials));
         keep_trials = keep_trials(~cellfun('isempty',keep_trials)); keep_trials = vertcat(keep_trials{:});
-        baseline = mean(keep_trials(:,1:66),2);
-        keep_cramp = mean(keep_trials(:,win),2) - baseline; % keep the CRamp values that satisfy criterion
-        fullCR = baseline + keep_cramp; % unmasked CR
+        keep_baseline = mean(keep_trials(:,1:66),2);
+        keep_cramp = mean(keep_trials(:,win),2) - keep_baseline; % keep the CRamp values that satisfy criterion
+        fullCR = keep_baseline + keep_cramp; % unmasked CR
     % Blue rig throughout training
     elseif strcmp(rig,'blue') == 1 
         win = [47 48 49 50]; % determined through imageSubtraction.m
@@ -32,9 +32,9 @@ if numel(unique(rig)) == 1
         keep_trials_trialspace = keep_trials;
         keep_trials_idx = find(~cellfun('isempty',keep_trials));
         keep_trials = keep_trials(~cellfun('isempty',keep_trials)); keep_trials = vertcat(keep_trials{:});
-        baseline = mean(keep_trials(:,1:10),2);
-        keep_cramp = mean(keep_trials(:,win),2) - mean(keep_trials(:,1:10),2); % keep the CRamp values that satisfy criterion
-        fullCR = baseline + keep_cramp; % unmasked CR
+        keep_baseline = mean(keep_trials(:,1:10),2);
+        keep_cramp = mean(keep_trials(:,win),2) - keep_baseline; % keep the CRamp values that satisfy criterion
+        fullCR = keep_baseline + keep_cramp; % unmasked CR
     end 
     
 % If different rigs throughout training    
@@ -42,7 +42,7 @@ elseif numel(unique(rig)) > 1
     
     % Keep the trials
     keep_trials = cell(1,length(files));
-    baseline = cell(1,length(files));
+    keep_baseline = cell(1,length(files));
     fullCR = cell(1,length(files));
     cramp = {};
     
@@ -62,7 +62,7 @@ elseif numel(unique(rig)) > 1
             end
             for jj = 1:size(keep_trials{k},1)
                 keep_trials_temp_temp = keep_trials{k};
-                baseline{k} = mean(keep_trials_temp_temp(:,1:66),2);
+                keep_baseline{k} = mean(keep_trials_temp_temp(:,1:66),2);
             end
         % Fill this cell if on the blue rig on this session
         elseif strcmp(rig{k},'blue') == 1
@@ -79,7 +79,7 @@ elseif numel(unique(rig)) > 1
             end
             for jj = 1:size(keep_trials{k},1)
                 keep_trials_temp_temp = keep_trials{k};
-                baseline{k} = mean(keep_trials_temp_temp(:,1:10),2);
+                keep_baseline{k} = mean(keep_trials_temp_temp(:,1:10),2);
             end
         end  
     end
@@ -98,12 +98,12 @@ elseif numel(unique(rig)) > 1
                 if strcmp(rig{ses(ii)},'black') == 1
                     keep_cramp{ses(ii)} = mean(keep_trials_temp(:,win{ses(ii)}),2) - mean(keep_trials_temp(:,1:66),2);
                     for jj = 1:size(keep_trials{k},1)
-                        fullCR{k}(jj) = baseline{k}(jj) + keep_cramp{k}(jj);
+                        fullCR{k}(jj) = keep_baseline{k}(jj) + keep_cramp{k}(jj);
                     end
                 elseif strcmp(rig{ses(ii)},'blue') == 1
                     keep_cramp{ses(ii)} = mean(keep_trials_temp(:,win{ses(ii)}),2) - mean(keep_trials_temp(:,1:10),2);
                     for jj = 1:size(keep_trials{k},1)
-                        fullCR{k}(jj) = baseline{k}(jj) + keep_cramp{k}(jj);
+                        fullCR{k}(jj) = keep_baseline{k}(jj) + keep_cramp{k}(jj);
                     end
                 end
             end
@@ -115,12 +115,12 @@ elseif numel(unique(rig)) > 1
             if strcmp(rig{k},'black') == 1
                 keep_cramp{k} = mean(keep_trials_temp(:,win{k}),2) - mean(keep_trials_temp(:,1:66),2);
                 for jj = 1:size(keep_trials{k},1)
-                    fullCR{k}(jj) = baseline{k}(jj) + keep_cramp{k}(jj);
+                    fullCR{k}(jj) = keep_baseline{k}(jj) + keep_cramp{k}(jj);
                 end
             elseif strcmp(rig{k},'blue') == 1
                 keep_cramp{k} = mean(keep_trials_temp(:,win{k}),2) - mean(keep_trials_temp(:,1:10),2);
                 for jj = 1:size(keep_trials{k},1)
-                    fullCR{k}(jj) = baseline{k}(jj) + keep_cramp{k}(jj);
+                    fullCR{k}(jj) = keep_baseline{k}(jj) + keep_cramp{k}(jj);
                 end
             end
         end
@@ -132,12 +132,12 @@ if nargout == 1
 elseif nargout == 4
     varargout{1} = keep_cramp;
     varargout{2} = keep_trials;
-    varargout{3} = baseline;
+    varargout{3} = keep_baseline;
     varargout{4} = fullCR;
 else
     varargout{1} = keep_cramp;
     varargout{2} = keep_trials;
-    varargout{3} = baseline;
+    varargout{3} = keep_baseline;
     varargout{4} = fullCR;
     varargout{5} = keep_trials_idx;
 end
