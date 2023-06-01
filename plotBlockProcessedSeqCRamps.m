5f375889a2afc783fcef08fc30d277b9791a04b2
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-06-01 08:59:05 -0400

    Update plotBlockProcessedSeqCRamps.m

--- plotBlockProcessedSeqCRamps.m
+++ plotBlockProcessedSeqCRamps.m
@@ -1,4 +1,4 @@
-function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_cramp,fullCR,mouse,blockSizeTemp,c)
+function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_cramp,keep_baseline,fullCR,mouse,blockSizeTemp,c)
 % Takes successful trials and their corresponding CR amplitudes, bins them 
 % into user-defined number of trial blocks while preserving the temporal 
 % structure of training, then finds the average within that trial block. 
@@ -8,6 +8,7 @@ function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_
 %       numBlocks = number of trial blocks 
 %       keep_trials_idx = index of trials that had a detected CR
 %       keep_cramp = CRamp values from those successful trials
+%       keep_baseline = baseline values from those successful trials
 %       fullCR = full CRamp values (baseline + keep_cramp) from those successful trials
 % OUTPUTS:
 %       blockCRamps = average CRamp at each trial block, 
@@ -16,12 +17,17 @@ function varargout = plotBlockProcessedSeqCRamps(numBlocks,keep_trials_idx,keep_
 %       blockFullCRamps = average full CRamp at each trial block,
 %           calculated using only CRamps from successful trials within a trial 
 %           block, preserving temporal structure of training
+%       blockBaseline = average baseline value at each trial block,
+%           calculated using only baseline values from successful trials within a trial 
+%           block, preserving temporal structure of training
 %       hf2 = plot binned CRamps using only successful CS-US trials while 
 %           preserving temporal structure of training
 %       hf3 = plot binned full CRamps only successful CS-US trials while 
 %           preserving temporal structure of training
 %       hf4 = plot each detected CRamp as it occurs throughout training
 %       hf5 = plot each full detected CRamp as it occurs throughout training
+%       hf6 = plot binned baseline values using only successful CS-US trials while 
+%           preserving temporal structure of training
 
 % Written by Kayla Fernando (3/28/23)
 
@@ -34,9 +40,11 @@ while n < numBlocks-1
     if isempty(idx)
         blockCRamps(n+1) = 0;
         blockFullCRamps(n+1) = 0;
+        blockBaseline(n+1) = 0;
     else
         blockCRamps(n+1) = mean(keep_cramp(idx(1):idx(end)));
         blockFullCRamps(n+1) = mean(fullCR(idx(1):idx(end)));
+         blockBaseline(n+1) = mean(keep_baseline(idx(1):idx(end)));
     end
     n = n+1;
 end
@@ -46,11 +54,13 @@ if n == numBlocks-1
     binRange = [n*100 n*100+100];
     idx = find(keep_trials_idx >= binRange(1) & keep_trials_idx <= binRange(2)); % includes right edge
     if isempty(idx)
-        blockCRamps(20) = 0;
-        blockFullCRamps(20) = 0;
+        blockCRamps(numBlocks) = 0;
+        blockFullCRamps(numBlocks) = 0;
+        blockBaseline(numBlocks) = 0;
     else
-        blockCRamps(20) = mean(keep_cramp(idx(1):idx(end)));
-        blockFullCRamps(20) = mean(fullCR(idx(1):idx(end)));
+        blockCRamps(numBlocks) = mean(keep_cramp(idx(1):idx(end)));
+        blockFullCRamps(numBlocks) = mean(fullCR(idx(1):idx(end)));
+        blockBaseline(numBlocks) = mean(keep_baseline(idx(1):idx(end)));
     end
 end
 
@@ -59,12 +69,12 @@ figure;
 ax1 = subplot(2,1,1);
     hf2 = plot(blockCRamps);
     title([mouse ' CRamps of detected CRs in sequential order']);
-    xlim([0 20]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
+    xlim([0 numBlocks]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
     ylim([0 1]); ylabel('Fraction of eye closed');
 ax2 = subplot(2,1,2);
     hf3 = plot(blockFullCRamps);
     title([mouse ' full CRamps of detected CRs in sequential order']);
-    xlim([0 20]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
+    xlim([0 numBlocks]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
     ylim([0 1]); ylabel('Fraction of eye closed');
 linkaxes([ax1, ax2], 'x');
 
@@ -73,25 +83,35 @@ figure;
 ax3 = subplot(2,1,1);
     hf4 = scatter(keep_trials_idx,keep_cramp,'.',c);
     title([mouse ' all CRamps of detected CRs in sequential order']);
-    xlim([0 2000]); xlabel('Trial #');
+    xlim([0 numBlocks*blockSizeTemp(1)]); xlabel('Trial #');
     ylim([0 1]); ylabel('Fraction of eye closed');
 ax4 = subplot(2,1,2);
     hf5 = scatter(keep_trials_idx,fullCR,'.',c);
     title([mouse ' all full CRamps of detected CRs in sequential order']);
-    xlim([0 2000]); xlabel('Trial #');
+    xlim([0 numBlocks*blockSizeTemp(1)]); xlabel('Trial #');
     ylim([0 1]); ylabel('Fraction of eye closed');
 linkaxes([ax3, ax4], 'x');
 
-if nargout == 2
+% Plot binned baseline value using only successful CS-US trials while preserving temporal structure of training
+figure;
+hf6 = plot(blockBaseline);
+title([mouse ' baseline value of detected CRs in sequential order']);
+xlim([0 numBlocks]); xlabel(['Trial block (' num2str(blockSizeTemp(1)) ' trials each)']);
+ylim([0 1]); ylabel('Fraction of eye closed');
+
+if nargout == 3
     varargout{1} = blockCRamps;
     varargout{2} = blockFullCRamps;
+    varargout{3} = blockBaseline;
 elseif nargout > 1
     varargout{1} = blockCRamps;
     varargout{2} = blockFullCRamps;
-    varargout{3} = hf2;
-    varargout{4} = hf3;
-    varargout{5} = hf4;
-    varargout{6} = hf5;
+    varargout{3} = blockBaseline;
+    varargout{4} = hf2;
+    varargout{5} = hf3;
+    varargout{6} = hf4;
+    varargout{7} = hf5;
+    varargout{8} = hf6;
 end
     
 end
