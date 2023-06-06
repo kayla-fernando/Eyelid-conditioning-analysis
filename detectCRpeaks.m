ab3bc2f3344d3664d7f4ffbe03f20a7888045a9e
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-06-06 14:38:11 -0400

    Update detectCRpeaks.m

--- detectCRpeaks.m
+++ detectCRpeaks.m
@@ -11,7 +11,7 @@ mouse = 'mouse';
 basepath = 'Y:\\home\kayla\Eyelid conditioning\';
 
 % Preprocess eyelid conditioning data, output promptData.txt
-eyelidPreprocess
+eyelidPreprocess; clear eyelid3_0_trials; clear eyelid3_5_trials; clear eyelid3_7_trials;
 
 % Define trial type to analyze
 if numel(unique(rig)) == 1
