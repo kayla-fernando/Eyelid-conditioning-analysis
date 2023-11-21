00a8827d22cba337da9789abf2b4c509e1486376
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2023-11-21 11:08:38 -0500

    Update forPlottingBehavior.m

--- forPlottingBehavior.m
+++ forPlottingBehavior.m
@@ -1,9 +1,9 @@
-SA(:,6) = mean(SA,2)
+data(:,end+1) = mean(data,2)
 
-for ii = 1:5
-    plot(SA(:,ii),'Color','[0.6350 0.0780 0.1840]')
+for ii = 1:size(data,2)-1
+    plot(data(:,ii),'Color','[0.6350 0.0780 0.1840]')
     hold on
 end
 
-plot(SA(:,end),'Color','r','LineWidth',2)
+plot(data(:,end),'Color','r','LineWidth',2)
 hold off
