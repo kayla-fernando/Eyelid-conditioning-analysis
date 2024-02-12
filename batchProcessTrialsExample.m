33652c3522a60bf6cc455338d63f4b2ef51b0d7b
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2024-02-12 11:17:28 -0500

    Add files via upload

---
+++ batchProcessTrialsExample.m
@@ -0,0 +1,40 @@
+%%
+
+close all
+clear all
+clc
+
+experiment = 'Thy1-ChR2';
+
+mouse = 'KF186';
+rig = 'blue';
+date = '240212';
+
+if strcmp(rig,'black') == 1
+    win = [139 140 141 142]; %determined through imageSubtraction.m
+elseif strcmp(rig,'blue') == 1 
+    win = [47 48 49 50]; %determined through imageSubtraction.m
+end
+
+isi = 250;
+session = 1;
+us = 3;
+cs = 5;
+
+folder = fullfile(['Z:\\home\kayla\Eyelid conditioning\' experiment '\' mouse '\' date]);
+trials = processTrials(fullfile(folder,'compressed'),...
+    fullfile(folder,'compressed',sprintf('%s_%s_s01_calib.mp4',mouse,date))); 
+
+save(fullfile(folder, 'trialdata.mat'),'trials');
+
+if numel(unique(trials.session_of_day)) == 1
+    [hf1,hf2,CRprob,CRamp]=makePlots(trials,rig,win,isi,session,us,cs);
+else
+    [hf1,hf2,CRprob,CRamp]=makePlots_mSessions(trials,rig,win,isi,session,us,cs);
+end
+
+hgsave(hf1,fullfile(folder,'CRs.fig'));
+hgsave(hf2,fullfile(folder,'CR_amp_trend.fig'));
+
+print(hf1,fullfile(folder,sprintf('%s_%s_CRs.pdf',mouse,date)),'-dpdf')
+print(hf2,fullfile(folder,sprintf('%s_%s_CR_amp_trend.pdf',mouse,date)),'-dpdf')
