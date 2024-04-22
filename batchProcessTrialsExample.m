d4b34e26a2451d6e47dad71c87d47c5639a2b8e8
Kayla Fernando <97700813+kayla-fernando@users.noreply.github.com>
2024-04-22 10:38:06 -0400

    Update batchProcessTrialsExample.m

--- batchProcessTrialsExample.m
+++ batchProcessTrialsExample.m
@@ -38,3 +38,7 @@ hgsave(hf2,fullfile(folder,'CR_amp_trend.fig'));
 
 print(hf1,fullfile(folder,sprintf('%s_%s_CRs.pdf',mouse,date)),'-dpdf')
 print(hf2,fullfile(folder,sprintf('%s_%s_CR_amp_trend.pdf',mouse,date)),'-dpdf')
+
+% Delete the videos that were just generated to save space
+cd(fullfile(folder,'compressed'))
+delete *.mp4
